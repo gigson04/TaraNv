71eca39c73f24f2873c15e802e4ccfeb5b78bd79
Gigson Aquino <gigsonaquino4@gmail.com>
2026-08-03 20:38:13 +0800

    Update public.js

--- public.js
+++ public.js
@@ -1,7 +1,5 @@
 /* ============================================================
    PUBLIC SITE LOGIC (index.html)
-
-
    Relies on shared.js being loaded first (config, data, utilities)
    ============================================================ */
 let currentCategory = "all";
